e6c7c732671e3bdaf65d3cebde850e12efb1d563
Ray <ray.li.verified@gmail.com>
2019-05-20 13:32:38 -0500

    Back Helper Platform Specific Wrapper
    *Wrap Back Helper in platform specific logic because iOS has no back button.

--- lib/ui/back_helper.dart
+++ lib/ui/back_helper.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_android/bloc/BlocProvider.dart';
 import 'package:flutter_android/main.dart';
@@ -12,27 +14,33 @@ class BackHelper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AppBloc appBloc = BlocProvider.of<AppBloc>(context);
-    return WillPopScope(
-      child: child,
-      onWillPop: () {
-        //Flutter app cannot go back anymore.
-        //Send back event to device for handling.
-        if (!Navigator.canPop(context)) {
-          appBloc.updateBack(false);
-          appBloc.navigate(Navigation.BACK);
+    //If platform is Android, capture and handle back events.
+    if (Platform.isAndroid) {
+      return WillPopScope(
+        child: child,
+        onWillPop: () {
+          //Flutter app cannot go back anymore.
+          //Send back event to device for handling.
+          if (!Navigator.canPop(context)) {
+            appBloc.updateBack(false);
+            appBloc.navigate(Navigation.BACK);
+            return Future<bool>.value(false);
+          }
+
+          //Flutter app can still go back. Navigate back.
+          Navigator.pop(context);
+          //Update back status. Can the Flutter app keep going back?
+          bool canPop = Navigator.canPop(context);
+          appBloc.updateBack(canPop);
+          print("Back Status New: " + canPop.toString());
+
+          //Return false because navigation has been handled manually.
           return Future<bool>.value(false);
-        }
-
-        //Flutter app can still go back. Navigate back.
-        Navigator.pop(context);
-        //Update back status. Can the Flutter app keep going back?
-        bool canPop = Navigator.canPop(context);
-        appBloc.updateBack(canPop);
-        print("Back Status New: " + canPop.toString());
-
-        //Return false because navigation has been handled manually.
-        return Future<bool>.value(false);
-      },
-    );
+        },
+      );
+    } else {
+      //Pass through widget on iOS to enable natural back navigation.
+      return child;
+    }
   }
 }
